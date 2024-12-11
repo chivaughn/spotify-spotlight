c741eea0308d5bba32b15aeea6cd00d9667d85e1
chivaughn <chivaughn.charles@my.uwi.edu>
2024-12-11 12:07:15 -0400

    Updated vercel file

--- server/index.js
+++ server/index.js
@@ -127,5 +127,5 @@ app.get("/refresh_token", function (req, res) {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
